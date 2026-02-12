44379b727256346da0d7f375fca363d37ec1fe70
parvathy2907 <parvathy896@gmail.com>
2026-02-12 23:00:11 +0530

    Remove duplicate route logic after merge

--- routes/assignmentRoutes.js
+++ routes/assignmentRoutes.js
@@ -21,25 +21,3 @@ router.get("/:id/map", getAssignmentMapData);
 router.patch("/:id/location", updateAssignmentLocation);
 
 module.exports = router;
-const express = require("express");
-const router = express.Router();
-
-const authMiddleware = require("../middleware/auth.js");
-
-const {
-  getAssignmentMapData,
-  updateAssignmentLocation,
-  getActiveAssignmentForVolunteer
-} = require("../controllers/assignmentController");
-
-/* 🔴 STATIC ROUTES FIRST */
-router.get(
-  "/volunteer-active",
-  getActiveAssignmentForVolunteer
-);
-
-/* 🔴 DYNAMIC ROUTES AFTER */
-router.get("/:id/map", getAssignmentMapData);
-router.patch("/:id/location", updateAssignmentLocation);
-
-module.exports = router;
