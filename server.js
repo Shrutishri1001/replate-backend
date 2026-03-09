153991d1100abe2a305d7bac15d15309029d27c6
parvathy2907 <parvathy896@gmail.com>
2026-03-09 18:18:01 +0530

    Fix CORS for Render frontend

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ const PORT = process.env.PORT ?? 5000;
 const app = express();
 
 // Middleware
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "https://replate-frontend.onrender.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
